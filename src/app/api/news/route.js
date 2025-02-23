d0c975f4de9d139186c2d93aa69bfb622633c54e
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-02-23 20:02:56 +0600

    news api route updated

--- src/app/api/news/route.js
+++ src/app/api/news/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export const GET = async (req) => {
   const newsCollection = await dbConnect(collections.newsCollection);
   const result = await newsCollection
-    .find({ category: { $ne: "features" } })
+    .find({ category: { $nin: ["features", "banner-news"] } })
     .sort({ published_date: -1 })
     .toArray();
   return NextResponse.json(result);
