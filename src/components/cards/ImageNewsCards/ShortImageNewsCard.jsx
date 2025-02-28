a16189612a58fa60a9f871dca2f381d069e25255
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-02-28 21:16:38 +0600

    ShortImageNewsCard added

---
+++ src/components/cards/ImageNewsCards/ShortImageNewsCard.jsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const ShortImageNewsCard = ({ news }) => {
+  const { _id, image, title, published_date } = news || {};
+  return (
+    <Link href={`/news/${_id}`} prefetch={true}>
+      <div className="h-full">
+        <div className="relative h-full overflow-hidden shadow-lg group">
+          <Image
+            src={image}
+            alt={title}
+            layout="responsive"
+            width={700}
+            height={900}
+            className="object-cover group-hover:scale-110 transition-transform duration-300 sm:min-h-full lg:max-h-[250px]"
+          />
+          {/* Dark Overlay */}
+          <div className="absolute inset-0 bg-black/50 group-hover:bg-black/60 transition-all"></div>
+          <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
+            <h2 className="text-white text-xl sm:text-lg lg:text-xl font-semibold">
+              {title}
+            </h2>
+            <div className="flex justify-between items-center mt-2 text-sm text-gray-300">
+              <span className="font-bold">{published_date.split("T")[0]}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ShortImageNewsCard;
--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -3,6 +3,7 @@ import dbConnect, { collections } from "@/lib/dbConnect";
 import Image from "next/image";
 import Link from "next/link";
 import LongImageNewsCard from "../cards/ImageNewsCards/LongImageNewsCard";
+import ShortImageNewsCard from "../cards/ImageNewsCards/ShortImageNewsCard";
 
 const Banner = async () => {
   // Get all banner_news from db --->
@@ -22,32 +23,7 @@ const Banner = async () => {
       </div>
       <div className="w-full md:w-full lg:w-5/12 flex flex-col md:flex-row lg:flex-col gap-6">
         {banner_news.slice(1, 3).map((news) => (
-          <Link href={`/news/${news._id}`} key={news._id} prefetch={true}>
-            <div className="h-full">
-              <div className="relative h-full overflow-hidden shadow-lg group">
-                <Image
-                  src={news.image}
-                  alt={news.title}
-                  layout="responsive"
-                  width={700}
-                  height={900}
-                  className="object-cover group-hover:scale-110 transition-transform duration-300 sm:min-h-full lg:max-h-[250px]"
-                />
-                {/* Dark Overlay */}
-                <div className="absolute inset-0 bg-black/50 group-hover:bg-black/60 transition-all"></div>
-                <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4">
-                  <h2 className="text-white text-xl sm:text-lg lg:text-xl font-semibold">
-                    {news.title}
-                  </h2>
-                  <div className="flex justify-between items-center mt-2 text-sm text-gray-300">
-                    <span className="font-bold">
-                      {news.published_date.split("T")[0]}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
+          <ShortImageNewsCard key={news._id} news={news} />
         ))}
       </div>
     </div>
