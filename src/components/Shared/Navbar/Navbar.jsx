87ee1bd6b2de6ba330cc839ea4a39b5534a9845a
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-02-28 22:16:35 +0600

    some updated

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import Login from "../../authentication/Login";
 import MobileNavbar from "./MobileNavbar";
 import { usePathname } from "next/navigation";
 
+
 const Navbar = () => {
   const path = usePathname();
   return (
