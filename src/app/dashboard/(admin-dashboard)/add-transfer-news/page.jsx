778208addd8eabe5e5f31323b67c8642e3eccfc1
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-03-07 23:53:33 +0600

    page header added in add-news & add-transfer news page

--- src/app/dashboard/(admin-dashboard)/add-transfer-news/page.jsx
+++ src/app/dashboard/(admin-dashboard)/add-transfer-news/page.jsx
@@ -1,8 +1,15 @@
+import { BiTransfer } from "react-icons/bi";
 import AddTransferNewsForm from "@/components/forms/AddTransferNewsForm";
+import DashboardPageHeader from "../../components/Dashboards/Header/DashboardPageHeader";
 
 const AddTransferNews = () => {
   return (
     <div>
+            <DashboardPageHeader
+        title="Add Transfer News"
+        subtitle="Quickly add and publish the latest transfer news to keep your platform up to date."
+        icon={BiTransfer}
+      />
       <AddTransferNewsForm />
     </div>
   );
--- src/app/dashboard/add-news/page.jsx
+++ src/app/dashboard/add-news/page.jsx
@@ -1,8 +1,15 @@
 import AddNewsForm from "@/components/forms/AddNewsForm";
+import DashboardPageHeader from "../components/Dashboards/Header/DashboardPageHeader";
+import { IoNewspaperSharp } from "react-icons/io5";
 
 const AddNewsPage = () => {
   return (
     <div>
+      <DashboardPageHeader
+        title="Add News"
+        subtitle="Quickly add and publish the latest news to keep your platform up to date."
+        icon={IoNewspaperSharp}
+      />
       <AddNewsForm />
     </div>
   );
