6e6ccdcefab281ec3b1533826a725b7b949a7928
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-03-10 22:10:08 +0600

    add-power-ranking post api route added

--- src/app/api/rankings/route.js
+++ src/app/api/rankings/route.js
@@ -7,3 +7,31 @@ export const GET = async (req) => {
   const result = await rankingsCollection.find().toArray();
   return NextResponse.json(result);
 };
+
+export const POST = async (req) => {
+  try {
+    const session = await auth();
+    if (!session || session?.user?.role !== "admin") {
+      return NextResponse.json({
+        status: 403,
+        error: "Forbidden Access",
+        message: "Only admins are allowed to perform this action!",
+      });
+    }
+
+    const ranking_data = await req.json();
+    const rankingsCollection = await dbConnect(collections.rankingsCollection);
+    const result = await rankingsCollection.insertOne(ranking_data);
+
+    return NextResponse.json(
+      { message: "Transfer news added successfully", data: result },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      error: "Internal Server Error",
+      message: error.message,
+    });
+  }
+};
